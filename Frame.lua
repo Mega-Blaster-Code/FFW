76ab63149478191f78d2185e739df480642b43c0
Mega <gabriel.cayres2009@gmail.com>
2025-02-17 18:22:54 -0300

    Update Frame.lua

--- Frame.lua
+++ Frame.lua
@@ -1,6 +1,3 @@
--- Frame For Widgets V0.2
--- Need Mega Drawing System to work.
-
 local module = {}
 
 GlobalLayer = 1
@@ -17,6 +14,7 @@ function buttons.new(name, x, y, w, h, color, text, layer, Zindex, clickedColor)
     name = name or "NIL"
     layer = layer or 1
     Zindex = Zindex or 1
+
     local button = setmetatable({
 
         name = name,
@@ -30,14 +28,39 @@ function buttons.new(name, x, y, w, h, color, text, layer, Zindex, clickedColor)
         Z = Zindex,
         Border = {
             Active = false,
-            BorderRadius = 0,
-            BorderColor = {0, 0, 0, 0},
+            Radius = 0,
+            Color = {0, 0, 0, 0},
+        },
+        Gradient = {
+            Active = false,
+            Color1 = {0, 0, 0, 255},
+            Color2 = {255, 255, 255, 255},
+            Rotation = 0
         }
 
     }, buttons)
+
     table.insert(buttons, button)
     module.updateZindex()
     return button
+end
+
+function buttons:optionals(Border, Gradient)
+    local BActive = Border.Active or false
+    local BRadius = Border.Radius or 0
+    local BColor = Border.Color or {255, 255, 255, 255}
+
+    self.Border.Active = BActive
+    self.Border.Radius = BRadius
+    self.Border.Color = BColor
+
+    local GActive = Gradient.Active or false
+    local GColor1 = Gradient.Color1 or {255, 255, 255, 255}
+    local GColor2 = Gradient.Color2 or {255, 255, 255, 255}
+    
+    self.Gradient.Active = GActive
+    self.Gradient.Color1 = GColor1
+    self.Gradient.Color2 = GColor2
 end
 
 function buttons:update()
@@ -61,14 +84,24 @@ function buttons:update()
 end
 
 function buttons:draw()
-    local position = self.position
-    local size = self.size
     local color = self.color
-    if nameClicked == self.name then
+
+    if self.hold then
         color = self.clickedColor
     end
-    ds.draw.rectangle(position.x, position.y, size.w, size.h, color, nil, nil, self.Border.BorderRadius, self.Border.BorderColor, self.Border.Active)
-    ds.draw.text(position.x, position.y, self.Z, {0, 0, 0, 255})
+    local color1 = self.Gradient.Color1
+    local color2 = self.Gradient.Color2
+
+    GradientShader:send("color1", ds.color.normal(color1))
+    GradientShader:send("color2", ds.color.normal(color2))
+    GradientShader:send("size", {self.size.w, self.size.h})
+    GradientShader:send("center", {self.size.w / 2 + self.position.x, self.size.h / 2 + self.position.y})
+    GradientShader:send("rotation", math.rad(self.Gradient.Rotation))
+
+    love.graphics.setShader(GradientShader)
+    ds.draw.rectangle(self.position.x, self.position.y, self.size.w, self.size.h, nil, nil, nil, self.Border.Radius, self.Border.Color, self.Border.Active)
+
+    ds.draw.text(self.position.x, self.position.y, self.Z)
 end
 
 function module.drawAll()
